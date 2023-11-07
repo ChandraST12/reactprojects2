99906c65e4b881d4bbde6770a6c19b1a815d661a
ChandraSt12 <chandrathakran615@gmail.com>
2023-11-07 21:14:40 +0530

    components

---
+++ Blog/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    // <div className="border-solid border-100 border-black border-t-0 border-r-0 w-0 h-0"></div>
+    <div>logo</div>
+
+  )
+}
+
+export default Logo
--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -1,7 +1,12 @@
-import Header from "./Header";
+import Header from "./header/Header";
 import Footer from "./Footer";
-
+import Container  from "../components/Container";
+import LogoutBtn from "./header/LogoutBtn";
+import Logo from '../components/Logo'
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
