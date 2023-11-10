9e40759be3512d8fe5d4c562c53d9dc2db6e1ccd
ChandraSt12 <chandrathakran615@gmail.com>
2023-11-10 11:54:39 +0530

    components finished

--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer";
 import Container  from "../components/Container";
 import LogoutBtn from "./header/LogoutBtn";
 import Logo from '../components/Logo'
+import RTE from "./RTE";
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
 }
